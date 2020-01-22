b0bb51d9887fce51a1b8569d47f771e3c82f217d
Kevin Nuricumbo <201245@ids.upchiapas.edu.mx>
2020-01-21 21:05:18 -0600

    Juego de dados

---
+++ 201245_juegoDados.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void datos();
+void verificarDados();
+void sumar();
+void status();
+
+int d1,d2, resultado;
+
+main(){
+datos();
+}
+
+void datos(){
+	cout<<"Ingrese el valor del dado 1"<<endl;
+	cin>>d1;
+	cout<<"Ingrese el valor del dado 2"<<endl;
+	cin>>d2;
+	verificarDados();
+}
+
+void verificarDados(){
+	if (d1>0 && d1<7){
+		if (d2>0 && d2<7){
+			sumar();
+		}
+		else{
+			cout<<"El valor del dado 2 es incorrecto"<<endl;
+			datos();
+		}
+	}
+	else{
+		cout<<"El valor del dado 1 es incorrecto"<<endl;
+		datos();
+	}
+}
+
+void sumar(){
+	resultado=d1+d2;
+	status();
+}
+
+void status(){
+	if (resultado==7 || resultado==11){
+		cout<<"Felicidades ha ganado"<<endl;
+	}
+	else{
+		if (resultado==2 || resultado==3 || resultado==12){
+			cout<<"Lo siento usted ha perdido"<<endl;
+		}
+		else{
+			cout<<"Tienes "<<resultado<<" puntos a favor, elija premio o siga tirando"<<endl;
+			datos();
+		}
+	}
+}
+
